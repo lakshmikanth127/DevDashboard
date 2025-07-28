c2b468da82dce72952866f693ff751eb0b5c079a
lakshmikanth127 <taidalalakshmikanth127@gmail.com>
2025-07-28 11:45:50 -0400

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
